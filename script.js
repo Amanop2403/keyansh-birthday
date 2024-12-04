646c2b755d4d997240d1c46b8c4b1b6afce3daca
Amanop2403 <amansachinshah@gmail.com>
2024-12-04 23:02:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Countdown Timer
-const birthday = new Date("December 4, 2024 23:00:00").getTime(); // Set your brother's birthday date
+const birthday = new Date("December 4, 2024 23:10:00").getTime(); // Set your brother's birthday date
 const countdownElement = document.getElementById("countdown");
 
 function updateCountdown() {
